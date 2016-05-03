0e0858b842c9d14515600797eb81288be4635791
John Goodman <johncurtisgoodman@gmail.com>
2016-05-02 22:49:40 -0400

    Add demo planets

--- 3dDemo.js
+++ 3dDemo.js
@@ -4,10 +4,10 @@ window.onload = function () {
   var canvas = document.getElementById("canvas");
 
   var G = 0.000000001;
-  var vPt = {x: 450, y: 450, z: 540};
+  var vPt = {x: 450, y: 450, z: 400};
 
-  var earth = new tycho.Mass(1, 60, 0.000055, 450, 200, -100, 0.5, -1, 1, "#4d4dff", "#000080", G);
-  var moon = new tycho.Mass(2, 30, 0.000055, 450, 300, -100, -4, 8, -8, "#e6e6e6", "#404040", G);
+  var earth = new tycho.Mass(1, 60, 0.000055, 450, 600, -100, 0.5, -1, 1, "#4d4dff", "#000080", G);
+  var moon = new tycho.Mass(2, 30, 0.000055, 450, 700, -100, -4, 8, -8, "#e6e6e6", "#404040", G);
   var bodies = [];
   bodies.push(earth);
   bodies.push(moon);
@@ -24,7 +24,10 @@ window.onload = function () {
     bodies.forEach(function(body) {
       body.move(bodies);
       body.sphereDraw(canvas, vPt);
+      bodies.forEach(function (body) {
+        body.pos.y-=0.36;
+      });
     });
 
-  }, 32);
+  }, 24bp);
 };
